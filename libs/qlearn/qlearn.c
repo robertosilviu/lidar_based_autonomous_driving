e5a6446361f97bddfe40fbe9fe303ebf5fda91b0
robertosilviu <popitanu@gmail.com>
2022-01-28 00:15:31 +0100

    add qlearn standard functions and variables

---
+++ libs/qlearn/qlearn.c
@@ -0,0 +1,140 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "qlearn.h"
+
+float frand(float xmin, float xmax) {
+	float range;
+
+	range = (xmax - xmin);
+
+	return (xmin + range*(float)rand()/RAND_MAX);
+}
+
+void ql_init(int ns, int na) {
+	int s, a;
+	n_states = ns;
+	n_actions = na;
+
+	if(n_states > MAX_STATES) {
+		printf("Number of states exceeds max limit!\n");
+		exit(1);
+	}
+
+	if(n_actions > MAX_ACTIONS) {
+		printf("Number of actions exceeds max limit!\n");
+		exit(1);
+	}
+
+	alpha = ALPHA0;
+	gam = GAMMA0;
+	ini_eps = EPSINI;
+	fin_eps = EPSFIN;
+	decay = DECAY0;
+
+	for(s = 0; s < n_states; s++) {
+		for(a = 0; a < n_states; a++) {
+			Q[s][a] = 0;
+		}
+	}
+
+}
+
+void ql_set_learning_rate(float lr) {
+	alpha = lr;
+	printf("Learning rate: alpha = %f\n", alpha);
+}
+
+void ql_set_discount_factor(float df) {
+	gam = df;
+	printf("Discount factor: gamma = %f\n", gam);
+}
+
+void ql_set_expl_range(float ini_e, float fin_e) {
+	ini_eps = ini_e;
+	fin_eps = fin_e;
+	printf("Eploration probability: ini_eps = %f, fin_eps = %f\n", ini_e, fin_e);
+}
+
+void ql_set_expl_decay(float d) {
+	decay = d;
+	printf("Exploration decayr: decay = %f\n", decay);
+}
+
+float ql_get_learning_rate() {
+	return alpha;
+}
+
+float ql_get_discount_factor() {
+	return gam;
+}
+
+float ql_get_expl_decay() {
+	return decay;
+}
+
+float ql_get_epsilon() {
+	return epsilon;
+}
+
+void ql_reduce_expl() {
+	norm_eps = decay*norm_eps;
+	epsilon = fin_eps + norm_eps*(ini_eps - fin_eps);
+}
+
+float ql_maxQ(int s) {
+	int a;
+	float m;
+
+	m = Q[s][0];
+
+	for(a = 1; a < n_states; a++) {
+		if (Q[s][a] > m)
+			m = Q[s][a];
+	}
+
+	return m;
+}
+
+int ql_best_action(int s) {
+	int a, ba;
+	float m;
+
+	m = Q[s][0];
+	ba = 0;
+
+	for(a = 1; a < n_states; a++) {
+		if (Q[s][a] > m) {
+			m = Q[s][a];
+			ba = a;
+		}
+	}
+
+	return ba;
+}
+
+int ql_egreedy_policy(int s) {
+	int ra, ba;
+	float x;
+
+	ba = ql_best_action(s);
+	ra = rand()%n_actions;
+	x = frand(0, 1);
+
+	if (x < epsilon)
+		return ra;
+	else 
+		return ba;
+}
+
+float ql_updateQ(int s, int a, int r, int snew) {
+	float q_target;	// target Q value
+	float td_err;	// TD error
+
+	q_target = r + gam*ql_maxQ(snew);
+	td_err = q_target - Q[s][a];
+	Q[s][a] = Q[s][a] + alpha*td_err;
+
+	return fabs(td_err);
+}
---
+++ libs/qlearn/qlearn.h
@@ -0,0 +1,48 @@
+#ifndef QLEARN_H_
+#define QLEARN_H_
+
+#define MAX_STATES 100
+#define MAX_ACTIONS 100
+#define ALPHA0	1.0						// default learning rate
+#define EPSINI 0.9						// initial exploration factor
+#define EPSFIN 0.1						// final exploration factor
+#define GAMMA0 0.9						// default discount factor
+#define DECAY0 0.95						// default epsilon decay rate
+
+static int n_states;
+static int n_actions;
+//static int goal_state;
+
+static float alpha;						// learning rate
+static float gam;						// discount factor
+static float decay;						// decay rate for epsilon
+static float norm_eps = 1.0;			// normal exploration probability
+static float ini_eps;					// initial exploration probability
+static float fin_eps;					// final exploration probability
+static float epsilon;					// actual exploration probability
+
+//----------------------------
+//	QL matrice
+//----------------------------
+static int Q[MAX_STATES][MAX_ACTIONS];
+
+void ql_init(int ns, int na);
+void ql_set_learning_rate(float lr);
+void ql_set_discount_factor(float df);
+void ql_set_expl_range(float ini_e, float fin_e);
+void ql_set_expl_decay(float d);
+
+float ql_get_learning_rate();
+float ql_get_discount_factor();
+float ql_get_expl_decay();
+float ql_get_epsilon();
+
+void ql_reduce_expl();
+float ql_maxQ(int s);
+int ql_best_action(int s);
+int ql_egreedy_policy(int s);
+float ql_updateQ(int s, int a, int r, int snew);
+
+float frand(float xmin, float xmax);
+
+#endif
